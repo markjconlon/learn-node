0cd7f7a0f3de5426bfc11d369d290a8ac2b1bb0c
markjconlon <markjconlon@gmail.com>
2017-11-26 20:25:37 -0500

    using async await to wait for the store to save in mongodb, currently will crash if there is an error though

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,4 +1,21 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
   console.log(req.name);
   res.render('index');
 };
+
+exports.addStore = (req, res) => {
+  // allows us to use same template for add and edit store
+  res.render('editStore', {title: 'Add Store 💩🚁'})
+}
+
+exports.createStore = async (req, res) => {
+  // passes request from body of the add store form with name etc.
+  // strict only allows the data we are looking for
+  const store = new Store(req.body);
+  // sends the data to mongoose and sends back if it was successful or errors
+  await store.save();
+  console.log('It worked!');
+}
