864383e9002d840d4d26c76889fcc487c9d30d05
markjconlon <markjconlon@gmail.com>
2017-11-27 19:38:56 -0500

    added /stores route to index.js (the routes file of node)  and wrapped it and '/' with catchErrors because we are using async/await in storeController

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -20,4 +20,11 @@ exports.createStore = async (req, res) => {
   const store = await (new Store(req.body)).save();
   req.flash('success', `Sucessfully Created ${store.name}. Care to leave a review?`);
   res.redirect(`/store/${store.slug}`);
+};
+
+
+exports.getStores = async (req, res) => {
+  // query database for a list of all stores
+  const stores = await Store.find();
+  res.render('stores', { title: 'Stores', stores: stores });
 }
